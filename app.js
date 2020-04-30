69e569d6244162dc5cd7aff5486a564eb2acdacc
Veronica Minciuna <veronicaminci@gmail.com>
2020-04-30 14:07:54 -0400

    WIP

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const { db } = require("./models");
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+const port = 1337;
+app.listen(port, () => console.log(`App listening in port ${port}`));
